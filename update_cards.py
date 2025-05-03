19eb93a19b1fa0c322ad4a60601b6b74ac8d4557
masa0612 <mgmjd336@gmail.com>
2025-05-04 03:41:14 +0900

    temp

--- update_cards.py
+++ update_cards.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import sys
 import json
 import requests
 from bs4 import BeautifulSoup
@@ -7,61 +8,72 @@ from bs4 import BeautifulSoup
 OUTPUT_FILE = "assets/characters.json"
 WIKI = "https://sweethomemaid.wikiru.jp/"
 
-def get_cards(filter_=None):
+COLOR_MAP = {
+  "pc_red.png": "赤",
+  "pc_blu.png": "青",
+  "pc_grn.png": "緑",
+  "pc_yel.png": "黄",
+  "pc_aqa.png": "水",
+  "pc_vio.png": "紫",
+}
+
+def get_cards():
     response = requests.get(WIKI+"?全キャラクター一覧")
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
 
     table = soup.find(id="sortabletable1")
-    rows = [x.find_all("td") for x in table.find("tbody").find_all("tr")]
-
-    if filter_:
-        for x in rows:
-            if filter_ == x[1].find("a").text:
-                break
-        rows = [x]
-
+    rows = table.find("tbody").find_all("tr")
     cards = []
     for row in rows:
-        card = {}
-        card["icon"] = WIKI+row[0].find("img")["data-src"]
-        card["character"] = row[0].text
-        name = row[1].find("a")
-        card["name"] = name.text
-        card["url"] = WIKI+name["href"][2:]
-        if "コラボ" in name.text or "ロイズ" in name.text:
-            if "の特権" not in name.text:
-                card["character"] = "その他"
-        card["series"] = get_series(card["name"])
-        card["rarity"] = int(row[2].get_text(strip=True)[1])
-        card["color"] = row[3].text
-
-        skill = row[4].find("img")
-        card["s_name"] = skill["title"]
-        #card["s_img"] = WIKI+skill["data-src"]
-        card["s_type"] = get_skill_type(card["s_name"])
-        card["booster_tag"] = get_skill_tag(card["s_name"])
-        card["cost"] = row[5].text
-        card["ct"] = row[6].text
-        status = get_status(card["url"])
-        card["red"] = status[0]
-        card["blue"] = status[1]
-        card["green"] = status[2]
-        card["yellow"] = status[3]
-        card["aqua"] = status[4]
-        card["violet"] = status[5]
-
-        for i in range(7,11):
-            ability = row[i].find("img")
-            if ability is None: break
-            card[f"a{i-6}"] = ability["title"]
-            #card[f"a{i-6}_img"] = WIKI+ability["data-src"]
-        card["a_tag"] = get_ability_tag(card)
-        card["k_tag"], card["plus"]= get_killer_tag(card)
-
-        cards.append(card)
-
+        cards.append(get_card_info(WIKI+row.find("a").get("href")))
     return cards
+
+def get_card_info(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    ie5s = soup.find_all(class_="ie5")
+
+    char_table = ie5s[5]
+    status_table = ie5s[6]
+    skill1_table = ie5s[7]
+    skill2_table = ie5s[8]
+    ability_tables = ie5s[9:]
+
+    card = dict()
+    rows = char_table.find_all("tr")
+    card["name"] = rows[0].th.text
+    character = re.search(r'】(.+)', card["name"]).group(1)
+    if "コラボ" in card["name"] or "ロイズ" in card["name"]:
+        if "の特権" not in character:
+            card["character"] = "その他"
+    else:
+        card["character"] = character
+    card["series"] = get_series(card["name"])
+    card["rarity"] = int(rows[1].find_all("td")[1].find("a").get_text(strip=True)[1])
+    card["color"] = COLOR_MAP[rows[1].find("img").get("alt")]
+
+    rows = skill1_table.find_all("tr")
+    skill_name = rows[0].find("strong").get_text(strip=True)
+    card["s_type"] = get_skill_type(skill_name)
+    card["b_tag"] = get_skill_tag(skill_name)
+
+    rows = skill2_table.find_all("tr")
+    card["soon_flag"] = add_soon_flag(card["name"])
+    card["ct"] = get_ct(soup.find(class_="tag").find_all("a"), card["name"])
+    a_tag, k_tag, plus, multiplier = parse_ability(ability_tables)
+    card.update({"a_tag": a_tag, "k_tag": k_tag, "plus": plus})
+
+    status = get_status(status_table, multiplier)
+    card["red"] = status[0]
+    card["blue"] = status[1]
+    card["green"] = status[2]
+    card["yellow"] = status[3]
+    card["aqua"] = status[4]
+    card["violet"] = status[5]
+
+    return card
 
 def get_series(name):
     match = re.search(r'(【.*?】)', name)
@@ -78,70 +90,6 @@ def get_series(name):
     if "恐怖" in match.group(1):
         return "【ハロウィン】"
     return match.group(1)
-
-def get_killer_tag(card):
-    k_tag = set()
-    k_booster = list()
-    for i in range(1,5):
-        ability = card.get(f"a{i}")
-        if ability is None: break
-        target = ["キラー","サポート","なつき"]
-        if not any(x in ability for x in target): continue
-
-        match = re.search(r"(.+?)[＋+](\d+)[（(](.+?)[）)]", ability)
-        k_tag.add(match.group(1))
-        plus = match.group(2)
-        booster = match.group(3)
-        k_booster.append("+"+plus)
-        if booster == "全ブースター":
-            k_booster.append("ロケット+"+plus)
-            k_booster.append("ミサイル+"+plus)
-            k_booster.append("ボム+"+plus)
-        else:
-            k_booster.append(booster+"+"+plus)
-    return list(k_tag),k_booster
-
-def get_ability_tag(card):
-    a_tag = []
-    for i in range(1,5):
-        ability = card.get(f"a{i}")
-        if ability is None:
-            break
-
-        if "初期" in ability:
-            if "ペア" in ability:
-                a_tag.append("ペアルック初期スキルゲージ増加")
-            elif "シミラー" in ability:
-                a_tag.append("シミラー初期スキルゲージ増加")
-            else:
-                a_tag.append("初期スキルゲージ増加")
-        if "時スキルゲージ" in ability:
-            a_tag.append("他カードスキル使用時スキルゲージ増加")
-        if "時スキルクール" in ability:
-            a_tag.append("他カードスキル使用時スキルクールタイム減少")
-        if "アクセントカラー" in ability:
-            a_tag.append("アクセントカラー")
-            if "全" in ability:
-                a_tag.append("アクセントカラー(全)")
-            else:
-                a_tag.append(ability)
-        if "手数" in ability:
-            a_tag.append(ability)
-        if "リバウンド" in ability:
-            a_tag.append("リバウンドスキルコスト")
-        if "ダイエット" in ability:
-            a_tag.append("ダイエットスキルコスト")
-        if "リンケージス" in ability:
-            a_tag.append("リンケージスキルコスト減少")
-        if "ペア" in ability and "減少" in ability:
-            a_tag.append("ペアルックスキルコスト減少")
-        if "シミラー" in ability and "減少" in ability:
-            a_tag.append("シミラースキルコスト減少")
-        if "リンケージオ" in ability:
-            a_tag.append("リンケージオールスコアアップ")
-        if "貫通" in ability:
-            a_tag.append("ピーナッツ貫通ロケット")
-    return a_tag
 
 def get_skill_type(s_name):
     if "クリエイト" in s_name:
@@ -181,45 +129,7 @@ def get_skill_tag(s_name):
         s_tag.append("ミックス")
     return s_tag
 
-def get_status(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
-    content_div = soup.find(id="content_1_1")
-    sibling_div = content_div.find_next_sibling("div")
-    table = sibling_div.find("table")
-    rows = table.find_all("tr")
-    status_list = []
-    for x in rows[2:]:
-        cols = x.find_all("td")
-        status_list.append(int(cols[2].get_text(strip=True).replace(",","")))
-    max_value = max(status_list)
-    multiplier = get_multiplier(soup)
-    return [int(x*multiplier) if x == max_value else x for x in status_list]
-
-def get_multiplier(soup):
-    div = soup.find(id="content_1_3")
-    multiple = int()
-    color = str()
-    while True:
-        div = div.find_next_sibling("div")
-        if div is None: break
-        if "ie5" not in div["class"]: continue
-
-        table = div.find("table")
-        rows = table.find_all("tr")
-        for x in range(0,(int(len(rows)/2))):
-            ths = rows[x*2].find_all("th")
-            ability = ths[1].text
-            if "カードスコア" not in ability:
-                continue
-
-            match = re.search(r'(\d+)', ability)
-            multiple += int(match.group(1) if match else None)
-
-    return float(multiple/100)+1.02
-
-def add_soon_flag(cards):
+def add_soon_flag(name):
     target = [
       "【D.C.4コラボ】有里咲",
       "【夢見るバニータイム】ひまり子",
@@ -244,13 +154,99 @@ def add_soon_flag(cards):
       "【二人きりのささやき耳かき】スカーレット",
       "【夢見る乙女のパジャマパーティー】花音",
     ]
-    for x in cards:
-        x["soon_flag"] = x["name"] in target
+    return name in target
+
+def get_ct(anchors, name):
+    ret = [x.text for x in anchors if "最小クールタイム" in x.text]
+    if len(ret) == 0:
+        print("Not exist ct tag:", name)
+        return 3
+    else:
+        return int(ret[0][-1])
+
+def get_status(table, multiplier):
+    rows = table.find_all("tr")
+    status_list = []
+    for x in rows[2:]:
+        cols = x.find_all("td")
+        status_list.append(int(cols[2].get_text(strip=True).replace(",","")))
+    max_value = max(status_list)
+    return [int(x*multiplier) if x == max_value else x for x in status_list]
+
+def parse_ability(ability_tables):
+    killer_target = ["キラー","サポート","なつき"]
+
+    multiple = int()
+    a_tag = list()
+    p_tag = list()
+    k_tag = list()
+    for a_table in ability_tables:
+        ability = a_table.find_all("th")[1].text
+        if "デッキスコア" in ability: continue
+        if ability == "スキルクールタイム1減少": continue
+
+        if "カードスコア" in ability:
+            match = re.search(r'(\d+)', ability)
+            multiple += int(match.group(1) if match else None)
+        elif any(x in ability for x in killer_target):
+            k,p= get_killer_tag(ability)
+            k_tag.append(k)
+            p_tag = p_tag + p
+        else:
+            a_tag.append(get_ability_tag(ability))
+
+    return a_tag,list(set(k_tag)),list(set(p_tag)),float(multiple/100)+1.02
+
+def get_ability_tag(ability):
+    if "初期" in ability:
+        if "ペア" in ability:
+            return "ペアルック初期スキルゲージ増加"
+        elif "シミラー" in ability:
+            return "シミラー初期スキルゲージ増加"
+        else:
+            return "初期スキルゲージ増加"
+    if "時スキルゲージ" in ability:
+        return "他カードスキル使用時スキルゲージ増加"
+    if "時スキルクール" in ability:
+        return "他カードスキル使用時スキルクールタイム減少"
+    if "アクセントカラー" in ability:
+        return "アクセントカラー"
+    if "手数" in ability:
+        return ability
+    if "リバウンド" in ability:
+        return "リバウンドスキルコスト"
+    if "ダイエット" in ability:
+        return "ダイエットスキルコスト"
+    if "リンケージス" in ability:
+        return "リンケージスキルコスト減少"
+    if "ペア" in ability and "減少" in ability:
+        return "ペアルックスキルコスト減少"
+    if "シミラー" in ability and "減少" in ability:
+        return "シミラースキルコスト減少"
+    if "リンケージオ" in ability:
+        return "リンケージオールスコアアップ"
+    if "貫通" in ability:
+        return "ピーナッツ貫通ロケット"
+
+def get_killer_tag(ability):
+    plus_tag = list()
+    match = re.search(r"(.+?)[＋+](\d+)[（(](.+?)[）)]", ability)
+    k_tag = match.group(1)
+    plus = match.group(2)
+    booster = match.group(3)
+    plus_tag.append("+"+plus)
+    if booster == "全ブースター":
+        plus_tag.append("ロケット+"+plus)
+        plus_tag.append("ミサイル+"+plus)
+        plus_tag.append("ボム+"+plus)
+    else:
+        plus_tag.append(booster+"+"+plus)
+    return k_tag,plus_tag
 
 if __name__ == "__main__":
-    #print(get_cards("【朝一の悦び】彩葉"))
-    #exit()
-    cards = get_cards()
-    add_soon_flag(cards)
-    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-        json.dump(cards, f, indent=2, ensure_ascii=False)
+    if len(sys.argv) > 1:
+        print(get_card_info(sys.argv[1]))
+    else:
+        cards = get_cards()
+        with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+            json.dump(cards, f, indent=2, ensure_ascii=False)
